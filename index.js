c979758785c963c360b3c40889a051ac12cacffb
Razibul-Alam <Razibul-Alam@users.noreply.github.com>
2021-01-30 08:19:01 +0600

    function done

--- index.js
+++ index.js
@@ -1,19 +1,56 @@
-function firstClass(name) {
-    var Input = document.getElementById('firstInput');
+function ticketHandaler(name, inpt) {
+    var Input = document.getElementById(inpt+'Input');
     input = parseFloat(Input.value);
-    if (name == 'decrease') {
-        if (input < 1) {
-            newinptvalue = 0
-        } else {
-            var newinptvalue = input - 1
-        }
-    }
-    if (name == 'increase') {
-        var newinptvalue = input + 1
-    }
+     if (name == 'decrease') {
+         if (input < 1) {
+             newinptvalue = 0
+         } else {
+             var newinptvalue = input - 1
+         }
+     }
+     if (name == 'increase') {
+         var newinptvalue = input + 1
+     }
 
     Input.value = newinptvalue
 }
+
+
+
+// function firstClass(name) {
+//     var Input = document.getElementById('firstInput');
+//     input = parseFloat(Input.value);
+//     if (name == 'decrease') {
+//         if (input < 1) {
+//             newinptvalue = 0
+//         } else {
+//             var newinptvalue = input - 1
+//         }
+//     }
+//     if (name == 'increase') {
+//         var newinptvalue = input + 1
+//     }
+
+//     Input.value = newinptvalue
+// }
+
+
+// function economyClass(name) {
+//     var Input = document.getElementById('economyinput');
+//     input = parseFloat(Input.value);
+//     if (name == 'decrease') {
+//         if (input < 1) {
+//             newinptvalue = 0
+//         } else {
+//             var newinptvalue = input - 1
+//         }
+//     }
+//     if (name == 'increase') {
+//         var newinptvalue = input + 1
+//     }
+
+//     Input.value = newinptvalue
+// }
 
 // var totalamount=newinptvalue*150;
 // var subtotal=document.getElementById('subtotal').innerText='$'+totalamount;
