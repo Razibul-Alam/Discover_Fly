4bdab7d546714031533f599acf8d41bce87dddff
Razibul-Alam <Razibul-Alam@users.noreply.github.com>
2021-01-30 16:50:23 +0600

    final commit

--- index.js
+++ index.js
@@ -1,56 +1,30 @@
 function ticketHandaler(name, inpt) {
-    var Input = document.getElementById(inpt+'Input');
+    var Input = document.getElementById(inpt + 'Input');
     input = parseFloat(Input.value);
-     if (name == 'decrease') {
-         if (input < 1) {
-             newinptvalue = 0
-         } else {
-             var newinptvalue = input - 1
-         }
-     }
-     if (name == 'increase') {
-         var newinptvalue = input + 1
-     }
-
-    Input.value = newinptvalue
+    if (name == 'decrease') {
+        if (input < 1) {
+            newinptvalue = 0
+        } else {
+            var newinptvalue = input - 1
+        }
+    }
+    if (name == 'increase') {
+        var newinptvalue = input + 1
+    }
+    Input.value = newinptvalue;
+    getInput();
 }
 
 
-
-// function firstClass(name) {
-//     var Input = document.getElementById('firstInput');
-//     input = parseFloat(Input.value);
-//     if (name == 'decrease') {
-//         if (input < 1) {
-//             newinptvalue = 0
-//         } else {
-//             var newinptvalue = input - 1
-//         }
-//     }
-//     if (name == 'increase') {
-//         var newinptvalue = input + 1
-//     }
-
-//     Input.value = newinptvalue
-// }
-
-
-// function economyClass(name) {
-//     var Input = document.getElementById('economyinput');
-//     input = parseFloat(Input.value);
-//     if (name == 'decrease') {
-//         if (input < 1) {
-//             newinptvalue = 0
-//         } else {
-//             var newinptvalue = input - 1
-//         }
-//     }
-//     if (name == 'increase') {
-//         var newinptvalue = input + 1
-//     }
-
-//     Input.value = newinptvalue
-// }
-
-// var totalamount=newinptvalue*150;
-// var subtotal=document.getElementById('subtotal').innerText='$'+totalamount;
+function getInput() {
+    var firstInput = document.getElementById('firstInput');
+    var economyInput = document.getElementById('economyInput');
+    var firstTicket = firstInput.value * 150;
+    var economyTicket = economyInput.value * 100;
+    var subtotal = (firstTicket + economyTicket);
+    document.getElementById('subtotal').innerText = '$' + subtotal;
+    var tax = subtotal * 0.1;
+    document.getElementById('tax').innerText = tax;
+    var grandTotal = subtotal + tax;
+    document.getElementById('grandtotal').innerText = grandTotal;
+}
